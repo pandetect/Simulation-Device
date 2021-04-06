f535d073c4406fff437c93c99626127b49cf2b18
Ufuk Bombar <ufukbombar@bordatech.com>
2021-04-06 20:01:50 +0300

    Fixed another bug

--- consumer.py
+++ consumer.py
@@ -14,47 +14,52 @@ if __name__ == '__main__':
     # parse arguments
     args = parser.parse_args()
 
-    print('Connecting to', args.addr)
+    print('Trying to connect to the server')
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((args.addr, args.p))
-        print('Connected')
+    while True:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((args.addr, args.p))
+                print('Connected to:', args.addr)
 
-        while True: # get packages forever
-            invoker = create_invoker()
+                while True: # get packages forever
+                    invoker = create_invoker()
 
-            s.sendall(invoker)
+                    s.sendall(invoker)
 
-            print('Sent invocation waiting for package', end='')
+                    print('Sent invocation waiting for package', end='')
 
-            package_header = s.recv(12)
+                    package_header = s.recv(12)
 
-            if not package_header: 
-                print('Closed connection')
-                break
+                    if not package_header: 
+                        print('\nClosed connection')
+                        break
 
-            package_header_index  = int.from_bytes(package_header[0:8], byteorder='big')
-            package_header_length = int.from_bytes(package_header[8:12], byteorder='big')
+                    package_header_index  = int.from_bytes(package_header[0:8], byteorder='big')
+                    package_header_length = int.from_bytes(package_header[8:12], byteorder='big')
 
-            print('Index:', package_header_index, 'Length:', package_header_length, end='')
+                    print('Index:', package_header_index, 'Length:', package_header_length, end='')
 
-            package_data_body = b''
-            package_data_remain = package_header_length + 1
+                    package_data_body = b''
+                    package_data_remain = package_header_length + 1
 
-            while True:
-                print('-', end='', sep='')
-                package_data_temp = s.recv(package_data_remain)
+                    while True:
+                        print('-', end='', sep='')
+                        package_data_temp = s.recv(package_data_remain)
 
-                if not package_data_temp: 
-                    print('Closed connection! Possibly failing')
-                    break
+                        if not package_data_temp: 
+                            print('\nClosed connection!')
+                            break
 
-                package_data_body += package_data_temp
-                package_data_remain -= len(package_data_temp)
+                        package_data_body += package_data_temp
+                        package_data_remain -= len(package_data_temp)
 
-                if package_data_remain == 0:
-                    break
-                
-            print(' Recieved', len(package_data_body) - 1, 'bytes')
+                        if package_data_remain == 0:
+                            break
+                        
+                    print(' Recieved', len(package_data_body) - 1, 'bytes')
 
-            time.sleep(0.250) # sleep 250 ms
+                    time.sleep(0.250) # sleep 250 ms
+        except ConnectionRefusedError as e:
+            time.sleep(0.100) # sleep for 100 ms
+            
